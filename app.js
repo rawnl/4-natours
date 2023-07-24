d4e8b3368c657fef6afb8567af385c3a367c6614
Raw NL <www.angyangelina654@gmail.com>
2023-07-25 01:27:56 +0200

    Uploading/processing images using multer/sharp

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
-    console.log(req.headers);
+    // console.log(req.headers);
     next();
 });
 
