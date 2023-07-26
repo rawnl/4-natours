32fe75a11197c9c68d677a68dadf687699755b47
Raw NL <www.angyangelina654@gmail.com>
2023-07-27 00:10:54 +0200

    Sending real emails using Brevo (formerly sendinblue)

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,73 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { htmlToText } = require('html-to-text');
+const Transport = require('nodemailer-brevo-transport');
 
-const sendEmail = async (options) => {
-    // 1. Create a transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstname = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Raw NL <${process.env.EMAIL_FROM}>`;
+    }
 
-    // 2. Define the email options
-    const mailOptions = {
-        from: 'Raw NL <rawnl@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Create transporter from Brevo (sendinblue) - API -> working
+            // return nodemailer.createTransport(
+            //     new Transport({ apiKey: process.env.API_KEY })
+            // );
 
-    // 3. Send the email
-    await transporter.sendMail(mailOptions);
+            // Create transporter from Brevo (sendinblue) - SMTP
+            return nodemailer.createTransport({
+                host: 'smtp-relay.brevo.com',
+                port: 587,
+                auth: {
+                    user: 'www.rawnl97@gmail.com',
+                    pass: `${process.env.BREVO_SMTP_KEY}`,
+                },
+            });
+        }
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
+
+    // Send the actual email
+    async sendEmail(template, subject) {
+        // 1. Render HTML based on a pug template
+        const html = pug.renderFile(
+            `${__dirname}/../views/emails/${template}.pug`,
+            {
+                firstname: this.firstname,
+                url: this.url,
+                subject,
+            }
+        );
+        // 2. Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText(html),
+        };
+        // 3. Create transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.sendEmail('welcome', 'Welcome to Natours');
+    }
+    async sendPasswordReset() {
+        await this.sendEmail(
+            'passwordReset',
+            'Your password reset token (valid for only 10 minutes'
+        );
+    }
 };
-
-module.exports = sendEmail;
