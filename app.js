e3e4d4b575599693d42de58ef9fefaf4fa459f46
Raw NL <www.angyangelina654@gmail.com>
2023-07-31 11:19:47 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,18 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS => Setting: Access-Control-Allow-Origin *
+app.use(cors());
+// api.natours.com - Front-end: natours.com
+// app.use(
+//     cors({
+//         origin: 'https:natours.com',
+//     })
+// );
+
+app.options('*', cors());
+// app.options('/api/v1/tours', cors());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
